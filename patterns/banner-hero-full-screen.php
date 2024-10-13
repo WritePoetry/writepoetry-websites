61b836370d4122c4543c1c6939aeacdb1d9c7d09
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-10-13 21:01:39 +0200

    Update front-page.html template with hero banner and subscribe CTA

--- patterns/banner-hero-full-screen.php
+++ patterns/banner-hero-full-screen.php
@@ -32,35 +32,48 @@
 <h2 class="wp-block-heading alignwide has-text-align-center has-xx-large-font-size" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--70);">The Swiss Knife for WordPress.</h2>
 <!-- /wp:heading -->
 
-<!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
+<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-right:0;padding-left:0"><!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons alignwide"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://github.com/giacomo-secchi/write-poetry/releases/latest" target="_blank" rel=" noreferrer noopener nofollow">Get WritePoetry Plugin ➜</a></div>
-<!-- /wp:button -->
-
-<!-- wp:button {"backgroundColor":"contrast","className":"is-style-fill"} -->
-<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-contrast-background-color has-background wp-element-button" href="https://github.com/giacomo-secchi/write-poetry/blob/trunk/CONTRIBUTING.md" target="_blank" rel=" noreferrer noopener nofollow">CONTRIBUTE to WritePoetry &lt;/&gt;</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://github.com/WritePoetry/wordpress-plugin/releases/latest" target="_blank" rel=" noreferrer noopener nofollow">Get WritePoetry Plugin ➜</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons -->
+
+
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:paragraph -->
+<p>Current version:&nbsp;</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:write-poetry/api-fetcher {"url":" https://api.github.com/repos/giacomo-secchi/write-poetry/releases/latest","text":"tag_name","link":"html_url"} -->
+<span data-url=" https://api.github.com/repos/giacomo-secchi/write-poetry/releases/latest" data-link="html_url" data-text="tag_name" class="wp-block-write-poetry-api-fetcher">Fetching data…</span>
+<!-- /wp:write-poetry/api-fetcher --></div>
+<!-- /wp:group -->
+ 
+
+</div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons alignwide"><!-- wp:button {"backgroundColor":"contrast","className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-contrast-background-color has-background wp-element-button" href="https://github.com/WritePoetry/wordpress-plugin/blob/trunk/CONTRIBUTING.md" target="_blank" rel=" noreferrer noopener nofollow">CONTRIBUTE to WritePoetry &lt;/&gt;</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+
 
 <!-- wp:spacer {"height":"0px","style":{"layout":{"flexSize":"100px","selfStretch":"fixed"}}} -->
 <div style="height:0px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#361C1C","openInNewTab":true,"showLabels":true,"size":"has-normal-icon-size","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
-<ul class="wp-block-social-links has-normal-icon-size has-visible-labels has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"https://github.com/giacomo-secchi/write-poetry","service":"github","label":"GitHub Repository","rel":""} /--></ul>
+<!-- wp:social-links {"iconColor":"contrast","openInNewTab":true,"showLabels":false,"size":"has-normal-icon-size","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links has-normal-icon-size has-visible-labels has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"https://github.com/WritePoetry/wordpress-plugin","service":"github","label":"WritePoetry GitHub organization account","rel":""} /--></ul>
 <!-- /wp:social-links -->
 
-<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-<div class="wp-block-group">
-	<!-- wp:paragraph -->
-	<p>Current version:</p>
-	<!-- /wp:paragraph -->
 
-	<!-- wp:html -->
-	<div class="api-fetch" data-api-url="https://api.github.com/repos/giacomo-secchi/write-poetry/releases/latest" data-api-link="html_url" data-api-text="tag_name"></div>
-	<!-- /wp:html -->
-</div>
-<!-- /wp:group -->
 
 
 
--- readme.txt
+++ readme.txt
@@ -18,7 +18,7 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 == Recommended Plugins ==
 
 Write Poetry
-https://github.com/giacomo-secchi/write-poetry
+https://github.com/WritePoetry/wordpress-plugin
 
 == Copyright ==
 
