02f624dca02122a43dd3fba23f9d0b68d489208d
Alberto Giusti <spaghetti_boys@hotmail.it>
2024-12-20 17:54:40 +0100

    New presentation banner with social links

---
+++ patterns/banner-2.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Title: Hero
+ * Slug: writepoetry-website/banner-2
+ * Categories: banner, call-to-action, featured
+ * Viewport width: 1400
+ */
+?>
+
+
+<!-- wp:group {"className":"banner-2","layout":{"type":"constrained"}} -->
+<div class="wp-block-group banner-2"><!-- wp:columns {"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<div class="wp-block-columns alignwide has-white-color has-text-color has-link-color"><!-- wp:column {"verticalAlignment":"center","width":"50%","style":{"border":{"radius":"4px","left":{"color":"var:preset|color|primary","width":"10px"}},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"backgroundColor":"secondary"} -->
+<div class="wp-block-column is-vertically-aligned-center has-secondary-background-color has-background" style="border-radius:4px;border-left-color:var(--wp--preset--color--primary);border-left-width:10px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);flex-basis:50%"><!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading">Do You want to know more about WritePoetry?</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>
+WritePoetry is like WordPress on steroids, or better it’s wp development without hustle.<br>
+
+WritePoetry is a WordPress plugin designed to assist developers in their daily tasks.<br>
+The purpose of this plugin is to streamline the developer's workflow, leaving them with the sole task of handling configurations.
+
+This is a WIP so a feedback on how to improve the tool would be highly appreciated.<br><br>
+
+
+<a style="text-transform: uppercase; float:right;" class="has-source-code-pro-font-family" href="<?php echo esc_url( home_url( 'project-presentation/' ) ); ?>" data-type="page">Read the Project Presentation</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column">
+    
+
+ 
+  
+
+<!-- wp:group {"style":{"background":{"backgroundImage":{"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/yancy-min-842ofHC6MaI-unsplash.jpg","source":"file","title":"yancy-min-842ofHC6MaI-unsplash"},"backgroundSize":"cover"},"border":{"radius":"4px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","flexWrap":"wrap","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
+<div class="wp-block-group" style="border-radius:4px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:social-links {"iconColor":"base","openInNewTab":true,"showLabels":true,"size":"has-large-icon-size","className":"has-visible-labels has-icon-color is-style-logos-only","style":{"spacing":{"margin":{"bottom":"0","top":"0","left":"0","right":"0"},"blockGap":{"top":"0","left":"0"}}},"layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap","orientation":"horizontal"}} -->
+<ul class="wp-block-social-links has-large-icon-size has-visible-labels has-icon-color is-style-logos-only" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">
+    <!-- wp:social-link {"url":"https://github.com/WritePoetry/wordpress-plugin","service":"github","label":"GitHub Organization","rel":""} /-->
+    </ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"background":{"backgroundImage":{"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/youtube-background.png","source":"file","title":"yancy-min-842ofHC6MaI-unsplash"},"backgroundSize":"cover"},"border":{"radius":"4px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","flexWrap":"wrap","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
+<div class="wp-block-group" style="border-radius:4px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:social-links {"iconColor":"base","openInNewTab":true,"showLabels":true,"size":"has-large-icon-size","className":"has-visible-labels has-icon-color is-style-logos-only","style":{"spacing":{"margin":{"bottom":"0","top":"0","left":"0","right":"0"},"blockGap":{"top":"0","left":"0"}}},"layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap","orientation":"horizontal"}} -->
+<ul class="wp-block-social-links has-large-icon-size has-visible-labels has-icon-color is-style-logos-only" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0"><!-- wp:social-link {"url":"https://www.youtube.com/@WritePoetrywithWordPress","service":"youtube","label":"YouTube Channel"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group -->
+
+
+
+</div>
+<!-- /wp:column -->
+
+
+
+</div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+ 
+<!-- wp:spacer {"height":"var(--wp--preset--spacing--90)"} -->
+<div style="height:var(--wp--preset--spacing--90)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
--- patterns/banner-hero-full-screen.php
+++ patterns/banner-hero-full-screen.php
@@ -6,61 +6,49 @@
  * Viewport width: 1400
  */
 ?>
-<!-- wp:group {"tagName":"main","style":{"dimensions":{"minHeight":"100vh"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center","flexWrap":"nowrap"}} -->
-<main class="wp-block-group" style="min-height:100vh;">
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center","flexWrap":"nowrap"}} -->
+<div class="wp-block-group " style="min-height:100vh;">
 
-<!-- wp:pattern {"slug":"writepoetry-website/logo-writepoetry"} /-->
+    <!-- wp:pattern {"slug":"writepoetry-website/logo-writepoetry"} /-->
 
-<!-- wp:heading {"textAlign":"center","align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|70"}}},"fontSize":"xx-large"} -->
-<h2 class="wp-block-heading alignwide has-text-align-center has-xx-large-font-size" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--70);">The Swiss Knife<br>for WordPress Developers.</h2>
-<!-- /wp:heading -->
+    <!-- wp:heading {"textAlign":"center","align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|70"}}},"fontSize":"xx-large"} -->
+    <h2 class="wp-block-heading alignwide has-text-align-center has-xx-large-font-size" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--70);">The Swiss Knife<br>for WordPress Developers.</h2>
+    <!-- /wp:heading -->
 
-<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
-<div class="wp-block-group alignwide"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide" style="padding-right:0;padding-left:0"><!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons alignwide"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://github.com/WritePoetry/wordpress-plugin/releases/latest" target="_blank" rel=" noreferrer noopener nofollow">Get WritePoetry Plugin -></a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons -->
+    <!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+    <div class="wp-block-group alignwide"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group alignwide" style="padding-right:0;padding-left:0">
+        
+        <!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
+        <div class="wp-block-buttons alignwide"><!-- wp:button -->
+        <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://github.com/WritePoetry/wordpress-plugin/releases/latest" target="_blank" rel=" noreferrer noopener nofollow">Get WritePoetry Plugin -></a></div>
+        <!-- /wp:button --></div>
+        <!-- /wp:buttons -->
 
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"fontSize":"small","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-<div class="wp-block-group has-small-font-size" style="margin-top:0;margin-bottom:0"><!-- wp:paragraph -->
-<p>Current version:</p>
-<!-- /wp:paragraph -->
+    <!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"fontSize":"small","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+    <div class="wp-block-group has-small-font-size" style="margin-top:0;margin-bottom:0"><!-- wp:paragraph -->
+    <p>Current version:</p>
+    <!-- /wp:paragraph -->
 
-<!-- wp:write-poetry/api-fetcher {"url":" https://api.github.com/repos/WritePoetry/wordpress-plugin/releases/latest","text":"tag_name","link":"html_url"} -->
-<span data-url=" https://api.github.com/repos/WritePoetry/wordpress-plugin/releases/latest" data-link="html_url" data-text="tag_name" class="wp-block-write-poetry-api-fetcher">Fetching data…</span>
-<!-- /wp:write-poetry/api-fetcher --></div>
-<!-- /wp:group -->
- 
+    <!-- wp:write-poetry/api-fetcher {"url":"https://api.github.com/repos/WritePoetry/wordpress-plugin/releases/latest","text":"tag_name","link":"html_url"} -->
+    <span data-url="https://api.github.com/repos/WritePoetry/wordpress-plugin/releases/latest" data-link="html_url" data-text="tag_name" class="wp-block-write-poetry-api-fetcher">Fetching data…</span>
+    <!-- /wp:write-poetry/api-fetcher -->
+    
+    </div>
+    <!-- /wp:group -->
+    
 
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons alignwide"><!-- wp:button {"backgroundColor":"contrast","className":"is-style-fill"} -->
+    <div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-contrast-background-color has-background wp-element-button" href="https://github.com/WritePoetry/wordpress-plugin/blob/trunk/CONTRIBUTING.md" target="_blank" rel=" noreferrer noopener nofollow">CONTRIBUTE to WritePoetry &lt;/&gt;</a></div>
+    <!-- /wp:button --></div>
+    <!-- /wp:buttons --></div>
+    <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
-
-<!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons alignwide"><!-- wp:button {"backgroundColor":"contrast","className":"is-style-fill"} -->
-<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-contrast-background-color has-background wp-element-button" href="https://github.com/WritePoetry/wordpress-plugin/blob/trunk/CONTRIBUTING.md" target="_blank" rel=" noreferrer noopener nofollow">CONTRIBUTE to WritePoetry &lt;/&gt;</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:group -->
-
-
-
-<!-- wp:social-links {"iconColor":"contrast","openInNewTab":true,"size":"has-normal-icon-size","className":"has-visible-labels has-icon-color is-style-logos-only","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}},"layout":{"type":"flex","justifyContent":"center"}} -->
-<ul class="wp-block-social-links has-normal-icon-size has-visible-labels has-icon-color is-style-logos-only" style="margin-bottom:var(--wp--preset--spacing--50)">
-    <!-- wp:social-link {"url":"https://github.com/WritePoetry/wordpress-plugin","service":"github","label":"WritePoetry GitHub organization account","rel":""} /-->
-    <!-- wp:social-link {"url":"https://www.youtube.com/@WritePoetrywithWordPress","service":"youtube","label":"WritePoetry YouTube Channel"} /-->
-</ul>
-<!-- /wp:social-links -->
-
-
-
-
-</main>
-<!-- /wp:group -->
-
-
-
 
 
 
